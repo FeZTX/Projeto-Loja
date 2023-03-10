b2e2a0f50506ab5ac0a5541a3acf97cefebd7644
Felipe Teixeira do Carmo <felipe_carmo@live.com>
2023-03-10 16:01:46 -0300

    Icones

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     </div>
     <nav class="navIcons">
       <i class="fa-solid fa-house fa-2x"></i>
+      <i class="fa fa-camera-retro fa-2x"></i>
       <i class="fa-solid fa-house fa-2x"></i>
       <i class="fa-solid fa-house fa-2x"></i>
       <i class="fa-solid fa-house fa-2x"></i>
@@ -23,7 +24,7 @@
 
 .sideIcons {
   height: 100vh;
-  width: 55px;
+  width: 85px;
   background-color: blueviolet;
 }
 
@@ -32,7 +33,7 @@
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 60px
+  height: 85px
 }
 
 .logo img {
@@ -43,15 +44,20 @@
 .navIcons {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 50px;
   align-items: center;
   justify-content: center;
-  margin-top: 70px;
+  height: calc(100vh - 85px);
 }
 
 .navIcons i {
   color: white;
   filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.37));
+  transition: 0.2s;
+}
+
+.navIcons i:hover {
+  margin-left: 10px;
 }
 
 </style>
