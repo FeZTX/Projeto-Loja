75ab43470178072f1d4e306c3ba99db462e82d1d
Felipe Teixeira do Carmo <felipe_carmo@live.com>
2023-03-25 11:21:29 -0300

    Segunda aba

--- src/App.vue
+++ src/App.vue
@@ -8,26 +8,30 @@
         <i class="fa-solid fa-house fa-2x"></i>
         <i class="fa-solid fa-shop fa-2x"></i>
         <i class="fa-regular fa-user fa-2x"></i>
-        <i class="fa-solid fa-house fa-2x"></i>
-        <i class="fa-solid fa-house fa-2x"></i>
+        <i class="fa-regular fa-pen-to-square fa-2x"></i>
+        <i class="fa-solid fa-gear fa-2x"></i>
       </nav>
     </div>
     <div class="sideSelection">
+      <div class="sideLogo">
+        <img src="./assets/InstagramLogo.webp" alt="LogoEnterprise" />
+      </div>
       <ul class="listUl">
-        <li class="listLi">
-          <i class="fa-solid fa-store"></i>
-          <span>Vender</span>
-        </li>
+        <router-link class="linkVenda" to="venda">
+          <li class="listLi">
+            <i class="fa-solid fa-store"></i>
+            <span>Vender</span>
+          </li>
+        </router-link>
         <li class="listLi">
           <i class="fa-solid fa-table"></i>
           <span>Listar vendas</span>
         </li>
       </ul>
     </div>
+    <router-view />
   </header>
-  <router-view />
 </template>
-
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
 
@@ -45,8 +49,9 @@
 
 .sideIcons {
   height: 100vh;
-  width: 75px;
+  min-width: 75px;
   background-color: blueviolet;
+  box-shadow: 3px 0px 4px rgba(0, 0, 0, 0.137);
 }
 
 .logo {
@@ -84,18 +89,36 @@
 
 .sideSelection {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  min-width: 200px;
   width: 200px;
   height: 100vh;
   box-shadow: 3px 0px 10px rgba(151, 151, 151, 0.24);
+}
+
+.sideLogo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  width: 100%;
+}
+
+.sideLogo img {
+  width: 60%;
 }
 
 .listUl {
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
+  width: 100% !important;
   /* border: 1px solid red; */
+}
+
+.linkVenda{
+  width: 100%;
 }
 
 .listLi {
@@ -106,6 +129,8 @@
   gap: 5px;
   height: 45px;
   cursor: pointer;
+  color: black;
+  text-decoration: none;
 }
 
 .listLi:hover {
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="main">
+        <div class="containerPreco">
+            <div class="preco">
+                <span>R$ 678</span>
+            </div>
+            <div class="dia"></div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+#main {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+}
+
+.containerPreco {
+    display: flex;
+    justify-content: space-around;
+    gap: 20px;
+    width: 100%;
+    height: 500px;
+    margin: 20px;
+}
+
+.preco {
+    max-width: 80%;
+    height: 100px;
+    background-color: white;
+    border-radius: 15px;
+    padding: 30px;
+
+}
+
+.dia {
+    width: 80%;
+    height: 100px;
+    background-color: white;
+    border-radius: 15px;
+
+}
+</style>
